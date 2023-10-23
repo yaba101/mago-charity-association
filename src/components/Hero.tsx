c91b08f7d170db2431b6652b595dc37d9cbe3668
Yeabsira Mekuria <yabumek46@gmail.com>
2023-10-23 15:52:56 +0300

    add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import DonationButton from "./DonationButton";
+import { FaStar } from "react-icons/fa";
+
+const Hero = () => {
+  const pink_gradiant =
+    "bg-gradient-to-r from-pink-300 to-pink-600 filter blur-[900px]";
+  const white_gradiant = "bg-white bg-opacity-60 filter blur-[750px]";
+  const blue_gradiant =
+    "bg-gradient-to-t from-transparent via-blue-800 to-transparent filter blur-[123px]";
+  const bg_discount_gradient = "bg-gradient-to-tr from-gray-700 to-indigo-900";
+  const text_gradient =
+    " bg-gradient-to-br from-teal-100 via-teal-200 to-teal-500 text-transparent bg-clip-text";
+
+  return (
+    <section id="home" className={`flex md:flex-row flex-col sm:py-16 py-6`}>
+      <div
+        className={`flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div
+          className={`flex flex-row items-center py-[6px] px-4 ${bg_discount_gradient} rounded-[10px] mb-2`}
+        >
+          <FaStar className="text-yellow-300" />
+          <p
+            className={`font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2`}
+          >
+            <span className="text-white">Empower</span> Generosity:{" "}
+            <span className="text-white">Your Click,</span> Their Hope.
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            Moga <br className="sm:block hidden" />{" "}
+            <span className={`${text_gradient}`}>Charity</span>{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <DonationButton />
+          </div>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Association
+        </h1>
+        <p
+          className={`font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5`}
+        >
+          Welcome to the Moga Charity {"Association's"} official website, where
+          compassion meets action. Our mission is to make a positive and lasting
+          impact on the lives of those in need, right here in the heart of Moga
+          and beyond.
+        </p>
+      </div>
+
+      <div
+        className={`flex-1 flex justify-center items-center md:my-0 my-10 relative`}
+      >
+        <Image
+          src={"/donation.png"}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+          width={500}
+          height={500}
+        />
+        <div
+          className={`absolute z-[0] w-[40%] h-[35%] top-0 ${pink_gradiant} `}
+        />
+        <div
+          className={`absolute z-[1] w-[80%] h-[80%] rounded-full ${white_gradiant} bottom-40`}
+        />
+        <div
+          className={`absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 ${blue_gradiant}`}
+        />
+      </div>
+
+      <div className={`ss:hidden flex justify-center items-center`}>
+        <DonationButton />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#00040f", // Your primary background color
+        secondary: "#00f6ff", // Your secondary accent color
+        dimWhite: "rgba(255, 255, 255, 0.7)", // A dimmed white color
+        dimBlue: "rgba(9, 151, 124, 0.1)", // A dimmed blue color
       },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Use the Poppins font family
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
