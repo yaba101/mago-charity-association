33e5db8ab62ae2fe5fb28b3cbacfde91e7a0bd14
Yeabsira Mekuria <63213576+yaba101@users.noreply.github.com>
2023-12-14 00:01:01 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className={`bg-primary flex justify-center items-start`}>
+    <div className={`bg-primary flex justify-center items-start h-screen`}>
       <div className="xl:max-w-[1280px] w-full">
         <Hero />
       </div>
